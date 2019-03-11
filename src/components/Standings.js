5c845c022bee32f81fbd505342f86b6768f96af3
Jason Howie <howiejason93@gmail.com>
2019-03-11 12:23:30 -0400

    add favicon and metadata

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Standings extends React.Component {
 	constructor() {
 		super();
-
 	}
+
 	render() {
+		let now = new Date();
+		let string = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
 		return (
-			<h1>Test</h1>
+			<div>
+				<nav>
+					<Link to="/">Home</Link>
+				</nav>
+				<h1>Stadings as of {string}</h1>
+			</div>
 			)
 	}
 }
