6d88c4f729884891c33cca88bd62af48b6e632b6
Jason Howie <howiejason93@gmail.com>
2019-03-26 17:33:09 -0400

    Updates

---
+++ precache-manifest.6bc591169cdfae963e3b420e237ff1c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc01fe325d28f7dd24e0e3e26798a8d1",
+    "url": "/react-mlb/static/media/ball.dc01fe32.jpg"
+  },
+  {
+    "revision": "b8850a20a950ba0fb4bb7174b265e020",
+    "url": "/react-mlb/static/media/avatar.b8850a20.png"
+  },
+  {
+    "revision": "883c62338baf7598b38f",
+    "url": "/react-mlb/static/js/runtime~main.883c6233.js"
+  },
+  {
+    "revision": "e130ec6ac9f0be7071ed",
+    "url": "/react-mlb/static/js/main.e130ec6a.chunk.js"
+  },
+  {
+    "revision": "4e694bfd930880d9248d",
+    "url": "/react-mlb/static/js/2.4e694bfd.chunk.js"
+  },
+  {
+    "revision": "e130ec6ac9f0be7071ed",
+    "url": "/react-mlb/static/css/main.a14e8416.chunk.css"
+  },
+  {
+    "revision": "4e694bfd930880d9248d",
+    "url": "/react-mlb/static/css/2.bb3d1bb4.chunk.css"
+  },
+  {
+    "revision": "1d6b2d3b55614a165dad6c44a8f2781b",
+    "url": "/react-mlb/index.html"
+  }
+];
